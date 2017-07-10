f65ba6626d2bd9af212094e7a40bf228bd78467b
ikilllliki <rinka_16n1200079@nnn.ed.jp>
2017-07-10 15:46:04 +0900

    　text4_45

--- higher.js
+++ higher.js
@@ -1,15 +1,31 @@
-// 高階関数
-function myHigherFunction (func) {
-    console.log ('myHigherFunction');
+// // 高階関数
+// function myHigherFunction (func) {
+//     console.log ('myHigherFunction');
 
-    func ();
+//     func ();
+// }
+
+// // （普通の）関数
+// function outsideFunction () {
+//     console.log ('outsideFunction');
+// }
+
+// // outsideFunction (); // 関数呼び出し
+
+// myHigherFunction (outsideFunction); // 関数渡し
+
+
+function arrayWalk(data,f) {
+    for (var key in data) {
+        f(data[key],key);
+    }
 }
 
-// （普通の）関数
-function outsideFunction () {
-    console.log ('outsideFunction');
+// コールバック関数
+function showElement(value,key) {
+    console.log(key + ':' + value);
 }
 
-// outsideFunction (); // 関数呼び出し
-
-myHigherFunction (outsideFunction); // 関数渡し
+var ary = [1,2,4,8,16];
+// arrayWalk(ary,showElement);
+ary.forEach (showElement);
