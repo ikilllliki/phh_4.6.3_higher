84ff3f777e3a8647bcc76fe0cf88847ce5a23b53
ikilllliki <rinka_16n1200079@nnn.ed.jp>
2017-07-10 14:18:16 +0900

    基本の高階関数

---
+++ higher.js
@@ -0,0 +1,15 @@
+// 高階関数
+function myHigherFunction (func) {
+    console.log ('myHigherFunction');
+
+    func ();
+}
+
+// （普通の）関数
+function outsideFunction () {
+    console.log ('outsideFunction');
+}
+
+// outsideFunction (); // 関数呼び出し
+
+myHigherFunction (outsideFunction); // 関数渡し
